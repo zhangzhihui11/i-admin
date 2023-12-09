42cf4bc01aafdc0a77fc5ac0d3f323a258b97b70
张志辉 <2673824370@qq.com>
2023-12-09 15:26:26 +0800

    feature:初始化gateway,并完成token校验过滤器

---
+++ admin-gateway/src/main/java/com/zzh/admin/gateway/filter/TokenFilter.java
@@ -0,0 +1,47 @@
+package com.zzh.admin.gateway.filter;
+
+import com.alibaba.fastjson2.JSON;
+import com.zzh.admin.common.base.constants.RequestHeader;
+import com.zzh.admin.common.base.entity.SimpleUser;
+import com.zzh.admin.common.base.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class TokenFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String requestPath = exchange.getRequest().getPath().value();
+        log.info("requestPath is {}", requestPath);
+        // 判断是否为白名单
+        List<String> tokens = exchange.getRequest().getHeaders().get(RequestHeader.TOKEN_KEY);
+        if (tokens == null || tokens.isEmpty()) {
+            throw new IllegalStateException("Token is empty");
+        }
+        String token = tokens.get(0);
+        if (null != token) {
+            Claims claims = JwtUtil.parseToken(token);
+            SimpleUser simpleUser = new SimpleUser();
+            simpleUser.setAccount(claims.get("userAccount", String.class));
+            simpleUser.setUserId(claims.get("userId", Long.class));
+            ServerHttpRequest newRequest = exchange.getRequest().mutate().header(RequestHeader.SIMPLE_USER_KEY, JSON.toJSONString(simpleUser)).build();
+            return chain.filter(exchange.mutate().request(newRequest).build());
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
