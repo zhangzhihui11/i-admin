55cb99eac93ffaf68526c8d58909e8da4af5b88f
张志辉 <2673824370@qq.com>
2023-12-11 10:36:12 +0800

    feature: 配置用户信息拦截器

--- admin-common/common-base/src/main/java/com/zzh/admin/common/base/configuration/WebMvcConfiguration.java
+++ admin-common/common-base/src/main/java/com/zzh/admin/common/base/configuration/WebMvcConfiguration.java
@@ -1,6 +1,21 @@
 package com.zzh.admin.common.base.configuration;
 
+import com.zzh.admin.common.base.interceptor.SimpleUserInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@ConditionalOnClass(value = {WebMvcConfigurer.class})
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SimpleUserInterceptor())
+                .addPathPatterns("/**")
+                .order(Ordered.HIGHEST_PRECEDENCE);
+    }
 }
