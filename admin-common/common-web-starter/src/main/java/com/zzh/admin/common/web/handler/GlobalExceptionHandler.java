03270c7da784a244cafb8cbd51ce699e05cef9cb
张志辉 <2673824370@qq.com>
2023-12-16 21:43:21 +0800

    feature-202312: 完善全局异常处理逻辑，新增BizException处理逻辑

--- admin-common/common-web-starter/src/main/java/com/zzh/admin/common/web/handler/GlobalExceptionHandler.java
+++ admin-common/common-web-starter/src/main/java/com/zzh/admin/common/web/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.zzh.admin.common.web.handler;
 
 import com.zzh.admin.common.base.enums.RespStatus;
+import com.zzh.admin.common.base.exception.BizException;
 import com.zzh.admin.common.base.response.Resp;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,10 +10,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Resp<Object> handleException(MethodArgumentNotValidException e) {
+    @ExceptionHandler(Exception.class)
+    public Resp<Object> handleException(Exception e) {
+        if (e instanceof MethodArgumentNotValidException) {
+            return handleMethodArgumentNotValidException((MethodArgumentNotValidException) e);
+        }
+        if (e instanceof BizException) {
+            return handleBizException((BizException) e);
+        }
+        return Resp.buildResp(10500, e.getMessage(), null);
+    }
+
+    public Resp<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         StringBuilder errorMessage = new StringBuilder();
         e.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append(";"));
         return Resp.buildResp(RespStatus.INVALID_INPUT.getCode(), errorMessage.toString(), null);
+    }
+
+    public Resp<Object> handleBizException(BizException e) {
+
+        return Resp.buildResp(e.getCode(), e.getMessage(), null);
     }
 }
