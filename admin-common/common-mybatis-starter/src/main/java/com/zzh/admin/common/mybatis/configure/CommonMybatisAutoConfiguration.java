bbef7704dcb6a0e1c5f8998b2179fdbd6415a27c
张志辉 <2673824370@qq.com>
2023-12-16 22:11:22 +0800

    feature-202312: mybatis-starter新增分页插件

--- admin-common/common-mybatis-starter/src/main/java/com/zzh/admin/common/mybatis/configure/CommonMybatisAutoConfiguration.java
+++ admin-common/common-mybatis-starter/src/main/java/com/zzh/admin/common/mybatis/configure/CommonMybatisAutoConfiguration.java
@@ -1,6 +1,10 @@
 package com.zzh.admin.common.mybatis.configure;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,4 +12,11 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan(basePackages = {"com.zzh.admin.common.mybatis.mapper"})
 @ComponentScan(basePackages = {"com.zzh.admin.common.mybatis"})
 public class CommonMybatisAutoConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));//如果配置多个插件,切记分页最后添加
+        return interceptor;
+    }
 }
