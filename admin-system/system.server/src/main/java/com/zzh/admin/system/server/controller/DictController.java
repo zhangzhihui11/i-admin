052dc29457ca7e034644587ad11cb5feffa0811d
张志辉 <2673824370@qq.com>
2023-12-25 23:04:50 +0800

    feature-202312: system服务介入swagger

--- admin-system/system.server/src/main/java/com/zzh/admin/system/server/controller/DictController.java
+++ admin-system/system.server/src/main/java/com/zzh/admin/system/server/controller/DictController.java
@@ -3,6 +3,8 @@ package com.zzh.admin.system.server.controller;
 import com.zzh.admin.system.sdk.entity.dto.DictDTO;
 import com.zzh.admin.system.sdk.entity.vo.DictVO;
 import com.zzh.admin.system.server.service.DictService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -14,16 +16,19 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/dict")
+@Api
 public class DictController {
 
     private final DictService dictService;
 
     @GetMapping()
+    @ApiOperation(value = "hello")
     public List<DictVO> getDict(@RequestParam List<String> types) {
         return dictService.getDictByType(types);
     }
 
     @PostMapping
+    @ApiOperation(value = "world")
     public Boolean insertDict(@RequestBody @Valid DictDTO dictDTO) {
         return dictService.insertDict(dictDTO);
     }
