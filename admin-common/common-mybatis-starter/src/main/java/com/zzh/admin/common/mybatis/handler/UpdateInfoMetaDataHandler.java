63447278737e259838c6c809f3838b31f54ed501
张志辉 <2673824370@qq.com>
2024-01-03 20:47:34 +0800

    feature-202401:mybatis-plus字段填充

---
+++ admin-common/common-mybatis-starter/src/main/java/com/zzh/admin/common/mybatis/handler/UpdateInfoMetaDataHandler.java
@@ -0,0 +1,30 @@
+package com.zzh.admin.common.mybatis.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UpdateInfoMetaDataHandler implements MetaObjectHandler {
+    /**
+     * 插入元对象字段填充（用于插入时对公共字段的填充）
+     *
+     * @param metaObject 元对象
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    /**
+     * 更新元对象字段填充（用于更新时对公共字段的填充）
+     *
+     * @param metaObject 元对象
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
